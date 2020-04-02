1a30294408127ebfea5f84c4ebddbe90187bf073
arnaldoquagliato <arnaldoquagliato@gmail.com>
2020-04-02 10:18:20 -0300

    Finish gameOfStones.py

--- General/gameOfStone.py
+++ General/gameOfStone.py
@@ -1,28 +1,14 @@
-testCases = input("Number of cases: ")
+testCases = int(input("Number of cases: "))
 
 count = 0
 
-player = "First"
-counter = 1
-
-def alterPlayer(counter){
-    if int(counter) % 2 == 0:
-        player = "First"
-        counter+=1
+while count < testCases:
+    numberOfStones = int(input("Quantity of Stones: "))
+    if( (numberOfStones % 7 == 1) or (numberOfStones % 7 == 0)):
+        print ("Second")
     else:
-        player = "Second"
-    counter+=1    
-}
-while count < int(testCases):
-    numberOfStones = input("Quantity of Stones: ")
-    while int(numberOfStones) < 2:
-        numberOfStones = int(numberOfStones)
-        if numberOfStones-5 > 0 and numberOfStones > 3:
-            numberOfStones = numberOfStones-5
-            alterPlayer(counter)
-        elif numberOfStones-2 > 0 and numberOfStones > 2:
-            numberOfStones = numberOfStones-2
-            alterPlayer(counter)
-            alterPlayer(counter)
+        print ("First")
+
+    count += 1
         
-        
+    
--- General/teste.py
+++
@@ -1,6 +0,0 @@
-x = 2
-
-if x % 2 == 0:
-    print("é par")
-else:
-    print("é impar")
