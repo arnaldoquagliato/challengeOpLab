7ce373eb414f762f1cca066b215b8150a4524fda
arnaldoquagliato <arnaldoquagliato@gmail.com>
2020-04-05 09:52:27 -0300

    Consuming API from twitter finished

--- General/twitterChallenger.py
+++ General/twitterChallenger.py
@@ -11,7 +11,7 @@ api = tweepy.API(auth)
 name = raw_input("Digit a word: ")
 
 cont = 0
-for result in api.search(q={name}, count = 10, result_type = "recent"):
+for result in api.search(q={name}, count = 10):
     print("Twitter number: {} " .format(cont))
     print (result.text + "\n")
     cont +=1
